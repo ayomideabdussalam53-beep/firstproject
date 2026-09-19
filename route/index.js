a3336da99b5cf413ac05c4bbb86efaac3bb0ae2d
ayomideabdussalam-beep <ayomideabdussalam53@gmail.com>
2026-09-19 16:41:04 +0100

    did some changes

--- route/index.js
+++ route/index.js
@@ -5,10 +5,11 @@ const mongoose = require("mongoose");
 
 
 const userRoute = require("./userRoutes.js");
-
+const compassString = process.env.COMPASS_STRING;
+const atlasString = process.env.ATLAS_STRING;
 const productRoute = require("./productRoute.js");
 mongoose
-  .connect("mongodb://localhost:27017/myDatabase")
+  .connect(atlasString)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
@@ -27,3 +28,6 @@ app.use("/products", productRoute);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+//mongodb+srv://ayomideabdussalam53_db_user:<db_password>@cluster0.tnstjtp.mongodb.net/?appName=Cluster0
+//const compassString = "mongodb://localhost:27017/myDatabase"
+//const atlasString = "mongodb+srv://ayomideabdussalam53_db_user:ajisafe@cluster0.tnstjtp.mongodb.net/?appName=Cluster0"
